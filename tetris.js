ff13a6263b7ce8856fc0f4d16329c09dfda82fd8
Nathan Harrenstein <nathan.harrenstein@avalara.com>
2018-02-06 13:34:34 -0800

    Listen for basic keyboard input.

--- tetris.js
+++ tetris.js
@@ -4,7 +4,6 @@ const borderSize = 1;
 
 let matrix = document.getElementById('tetris');
 let cellSize = Math.min((matrix.clientHeight - borderSize * 2) / rows, (matrix.clientWidth - borderSize * 2) / cells);
-
 matrix.style.position = 'relative';
 matrix.style.height = cellSize * rows + 'px';
 matrix.style.width = cellSize * cells + 'px';
@@ -23,6 +22,15 @@ function createMino(row, cell, backgroundColor) {
   return mino;
 }
 
+function createITetrimino() {
+  return [
+    createMino(1, 3, 'black'),
+    createMino(1, 4, 'black'),
+    createMino(1, 5, 'black'),
+    createMino(1, 6, 'black')
+  ]
+}
+
 function moveMino(mino, row, cell) {
   mino.style.top = cellSize * row + 'px';
   mino.style.left = cellSize * cell + 'px';
@@ -30,29 +38,110 @@ function moveMino(mino, row, cell) {
   mino.dataset.cell = cell;
 }
 
-function createITetrimino() {
-  let tetrimino = [];
-  tetrimino.push(createMino(1, 3, 'black'));
-  tetrimino.push(createMino(1, 4, 'black'));
-  tetrimino.push(createMino(1, 5, 'black'));
-  tetrimino.push(createMino(1, 6, 'black'));
-  return tetrimino;
+
+function getMinoRow(mino) {
+  return parseInt(mino.dataset.row);
 }
 
-let currentTetrimino = createITetrimino();
+function getMinoCell(mino) {
+  return parseInt(mino.dataset.cell);
+}
 
-let dropTimer = window.setInterval(drop, 1000);
 
-function drop() {
+function getMinoCells() {
+  return currentTetrimino.map(getMinoCell);
+}
+
+function getMinoRows() {
+  return currentTetrimino.map(getMinoRow);
+}
+
+
+function getMaxMinoCell() {
+  return Math.max(...getMinoCells());
+}
+
+function getMinMinoCell() {
+  return Math.min(...getMinoCells());
+}
+
+function getMaxMinoRow() {
+  return Math.max(...getMinoRows());
+}
+
+
+function canMoveTetriminoDown() {
+  return getMaxMinoRow() < rows - 1;
+}
+
+function canMoveTetriminoLeft() {
+  return getMinMinoCell() > 0 && canMoveTetriminoDown();
+}
+
+function canMoveTetriminoRight() {
+  return getMaxMinoCell() + 1 < cells && canMoveTetriminoDown();
+}
+
+
+function moveTetrimino(rowDelta, cellDelta) {
+  for (let mino of currentTetrimino) {
+    moveMino(mino, getMinoRow(mino) + rowDelta, getMinoCell(mino) + cellDelta);
+  }
+}
+
+
+function moveTetriminoDown() {
+  moveTetrimino(1, 0);
+}
+
+function moveTetriminoLeft() {
+  moveTetrimino(0, -1);
+}
+
+function moveTetriminoRight() {
+  moveTetrimino(0, 1);
+}
+
+
+function handleKeyDown(e) {
   if (!currentTetrimino) {
     return;
   }
 
-  if (parseInt(currentTetrimino[0].dataset.row) < rows - 1) {
-    for (let mino of currentTetrimino) {
-      moveMino(mino, parseInt(mino.dataset.row) + 1, parseInt(mino.dataset.cell));
-    }
+  switch (e.key) {
+    case 'ArrowLeft':
+    case 'a':
+      if (canMoveTetriminoLeft())
+        moveTetriminoLeft();
+      break;
+    case 'ArrowRight':
+    case 'd':
+      if (canMoveTetriminoRight())
+        moveTetriminoRight();
+      break;
+    case 'ArrowDown':
+    case 's':
+      if (canMoveTetriminoDown())
+        moveTetriminoDown();
+      break;
+    default:
+      break;
+  }
+  
+}
+
+function handleDropTimerTick() {
+  if (!currentTetrimino) {
+    return;
+  }
+
+  if (canMoveTetriminoDown()) {
+    moveTetriminoDown();
   } else {
     currentTetrimino = null;
   }
 }
+
+let currentTetrimino = createITetrimino();
+document.addEventListener('keydown', handleKeyDown);
+let dropTimer = window.setInterval(handleDropTimerTick, 1000);
